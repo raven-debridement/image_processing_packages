1d92dd81ff77a9d7daa196d0a64c5cf2bdf3efe3
Jonathan Kim <jonkim93@gmail.com>
2013-06-19 09:37:55 -0700

    saving in case re-install of camera_umd messes up

---
+++ camera_umd/uvc_camera/camera_calibrate.sh
@@ -0,0 +1,1 @@
+rosrun camera_calibration cameracalibrator.py --size 10x7 --square 0.01229 right:=/right/image_raw left:=/left/image_raw right_camera:=/right left_camera:=/left
---
+++ camera_umd/uvc_camera/single_calibrate.sh
@@ -0,0 +1,1 @@
+rosrun camera_calibration cameracalibrator.py --size 10x7 --square 0.01229 image:=/camera/image_raw camera:=/camera
